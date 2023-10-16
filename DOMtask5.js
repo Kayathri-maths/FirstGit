afbbb0a3008bdfce2b6b22700123f1cf375f2606
Kayathri-maths <kayualagarsamy@gmail.com>
2023-10-16 19:19:29 +0530

    task5

---
+++ DOMtask5.js
@@ -0,0 +1,24 @@
+//getelementsbyclassname
+//var items=document.getElementsByClassName("list-group-item");
+//console.log(items);
+//console.log(items[3]);
+//items[2].textContent='Hello';
+//items[1].style.fontWeight='bold';
+//items[0].style.backgroundColor='yellow';
+//items[3].style.color='brown';
+//for(let i=0;i<items.length;i++){
+ //   items[i].style.backgroundColor='#f4f4f4';
+//}
+
+//getelementsbytagname
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[4]);
+li[2].textContent='Hello';
+li[1].style.fontWeight='bold';
+li[0].style.backgroundColor='yellow';
+li[3].style.color='brown';
+for(let i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
+}
+
