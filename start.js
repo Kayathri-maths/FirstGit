f368210539eb7fe57e660aa8acfb05717f5f75a9
Kayathri-maths <kayualagarsamy@gmail.com>
2023-10-16 12:06:19 +0530

    first commit

--- start.js
+++ start.js
@@ -1,3 +1,27 @@
-var headerTitle=document.getElementById("header-title");
+// Manipulate the title of the page and experiment with other things
+console.dir(document.getElementsByClassName);
+//console.log(document.title);
+//var headerTitle=document.getElementById("header-title");
 //console.log(headerTitle);
-headerTitle.textContent='Hello';
+//console.log(headerTitle.textContent); //Item Lister 123
+//console.log(headerTitle.innerText);  //Item Lister
+//headerTitle.textContent='Hello';
+//console.log(headerTitle.textContent); //Hello
+//console.log(headerTitle.innerText); //Hello
+//headerTitle.innerText='GoodBye';
+//console.log(headerTitle.innerText);//GoodBoy
+//console.log(headerTitle.textContent);//GoodBye
+//headerTitle.innerHTML='<h2>Hello</h2>';
+//headerTitle.style.borderBottom='solid 3px #000';
+//var header=document.getElementById("main-header");
+//header.style.borderBottom='solid 3px #000';
+
+// Make the title have a black border   
+var header=document.getElementById('main-header');
+header.style.border='solid 5px #000';
+
+//Now make ADD ITEM bold and chage the font color to greeen
+var Title=document.querySelector("h2");
+//console.log(Title.textContent);
+Title.style.fontWeight='bold';
+Title.style.color='green';
